5a21ca41b94df096a3ec33e1d95b7417a3fed8a2
Markus <markus@vogelvlug.de>
2024-10-25 20:11:57 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./templates/**/*.{html,js,ts,jsx,tsx}",
+    "./parts/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
