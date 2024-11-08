e1bb3e15b270bcf97742b393b8ac9c588b4aaed1
Markus <markus@vogelvlug.de>
2024-11-08 20:48:05 +0100

    Add dropdown menu

---
+++ src/components/Divider/Divider.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Divider as Component } from './Divider';
+
+const meta: Meta<typeof Component> = {
+  title: 'Fundamentals/Divider',
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const Divider: Story = {
+  args: {
+    className: "my-4"
+  },
+};
---
+++ src/components/DropdownMenu/DropdownMenu.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DropdownMenu as Component, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './DropdownMenu';
+import { Button } from '../Button/Button';
+
+const meta: Meta<typeof Component> = {
+  title: 'Navigation/DropdownMenu',
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const DropdownMenu: Story = {
+  args: {
+    children: (
+      <>
+        <DropdownMenuTrigger>
+          <Button variant="outline">Open Menu</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </>
+    ),
+  },
+};
