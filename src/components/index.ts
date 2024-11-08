0fc70d0a2a9238233b7a550733c2ed395d6ec5e5
Markus <markus@vogelvlug.de>
2024-11-08 14:05:07 +0100

    actually export collapsible

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from "./ClickableLogo/ClickableLogo";
 export * from "./NavMenu/NavMenu";
 export * from "./Select/Select";
 export * from "./Sheet/Sheet";
+export * from "./Collapsible/Collapsible";
