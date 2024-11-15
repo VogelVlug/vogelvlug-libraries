4847927888dfb3264b9710187736269def77891b
Markus <markus@vogelvlug.de>
2024-11-15 17:15:35 +0100

    Actually apply styles in the correct order

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -8,27 +8,28 @@ const typographyVariants = cva("inline-block", {
   variants: {
     variant: {
       body: "text-base font-normal",
-      h1: "text-4xl sm:text-3xl leading-tight font-bold text-title",
-      h2: "text-3xl sm:text-2xl leading-tight font-bold text-title",
-      h3: "text-2xl sm:text-xl leading-tight font-bold text-title",
-      h4: "text-xl sm:text-lg leading-tight font-bold text-title",
-      h5: "text-lg sm:text-base leading-tight font-bold text-title",
+      h1: "sm:text-4xl text-3xl leading-tight font-bold text-title",
+      h2: "sm:text-3xl text-2xl leading-tight font-bold text-title",
+      h3: "sm:text-2xl text-xl leading-tight font-bold text-title",
+      h4: "sm:text-xl text-lg leading-tight font-bold text-title",
+      h5: "sm:text-lg text-base leading-tight font-bold text-title",
       h6: "text-base leading-tight font-bold text-title",
       subtitle: "font-medium text-subtitle",
-      overtitle: "text-base font-medium uppercase tracking-wider text-overtitle",
+      overtitle:
+        "text-base font-medium uppercase tracking-wider text-overtitle",
       label: "text-sm font-medium text-caption",
       main: "text-base font-medium text-color-main",
-      accent: "text-base font-medium text-color-accent"
+      accent: "text-base font-medium text-color-accent",
     },
     align: {
       left: "text-left",
       center: "text-center",
-      right: "text-right"
-    }
+      right: "text-right",
+    },
   },
   defaultVariants: {
-    variant: "body"
-  }
+    variant: "body",
+  },
 });
 
 type TypographyProps = VariantProps<typeof typographyVariants> & {
