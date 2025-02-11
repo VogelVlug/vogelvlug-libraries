b695f1f2ffac7371c8b053790c71796f8379a3fa
Markus <markus@vogelvlug.de>
2025-02-11 17:15:51 +0100

    Ensure white is always used with color mode

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -49,7 +49,7 @@ export const NavMenuItem: React.FC<NavMenuItemProps> = ({
       <NavMenuPrimitive.Trigger className="flex items-center gap-1 font-semibold">
         {title} <ChevronDown className="size-4" />
       </NavMenuPrimitive.Trigger>
-      <NavMenuPrimitive.Content className="left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto">
+      <NavMenuPrimitive.Content className="left-0 top-0 w-full bg-default data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto">
         <NavMenuPrimitive.List className="flex flex-col gap-2 p-2">
           {children}
         </NavMenuPrimitive.List>
