40927d73cb8b82cc254594a316f47155fe0f9fcd
Markus <markus@vogelvlug.de>
2025-04-18 16:19:59 +0200

    Pass classes to header

--- packages/design-system/src/components/Header/Header.tsx
+++ packages/design-system/src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { cn } from "../../lib/utils";
 import { ClickableLogo } from "../ClickableLogo/ClickableLogo";
 import { Nav } from "./components/Nav";
 import { HeaderProps } from "./types";
 
-export const Header: React.FC<HeaderProps> = (props) => {
-  const { hideMenuOnPage = false } = props;
+export const Header: React.FC<HeaderProps & { className?: string }> = (
+  props,
+) => {
+  const { hideMenuOnPage = false, className } = props;
 
   if (hideMenuOnPage) return null;
 
   return (
-    <div className="bg-default sticky top-0 z-50">
+    <div className={cn("bg-default sticky top-0 z-50", className)}>
       <div className="mx-auto flex h-16 items-center justify-between px-8">
         <ClickableLogo />
         <div>
@@ -20,4 +23,3 @@ export const Header: React.FC<HeaderProps> = (props) => {
     </div>
   );
 };
-
